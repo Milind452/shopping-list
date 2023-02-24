f9ce21c51ba02dc4fb00144dad3c8e28a8b6d6d2
Milind452 <mohapatra.milind@gmail.com>
2023-02-24 12:54:20 +0530

    Update items context to re-render only when url changes

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -1,17 +1,31 @@
-import { createContext } from "react";
-
-import useFetch from "../hooks/useFetch";
+import { createContext, useCallback, useState } from "react";
 
 export const ItemsContext = createContext();
 
 export const ItemsContextProvider = ({ children }) => {
-    const [loading, error, data] = useFetch(
-        "https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items"
-    );
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [items, setItems] = useState([]);
+
+    const fetchItems = useCallback(async (listId) => {
+        try {
+            const data = await fetch(
+                `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/lists/${listId}/items`
+            );
+            const result = await data.json();
+            if (result) {
+                setItems(result);
+                setLoading(false);
+            }
+        } catch (e) {
+            setLoading(false);
+            setError(e.message);
+        }
+    }, []);
 
     return (
-        <ItemsContext.Provider value={{items: data, loading, error}}>
+        <ItemsContext.Provider value={{ items, loading, error, fetchItems }}>
             {children}
         </ItemsContext.Provider>
-    )
+    );
 };
