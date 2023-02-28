c037bb35b6d8aaa31586ea9af804a3856020d8cd
Milind452 <mohapatra.milind@gmail.com>
2023-02-28 23:01:44 +0530

    Use code splitting with lazy loading webpack chunk names

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import styled, { createGlobalStyle } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
 import AppContext from "./context/AppContext";
 
 import Header from "./components/Header/Header";
-import Lists from "./pages/Lists";
-import ListDetail from "./pages/ListDetail";
-import ListForm from "./pages/ListForm";
+const Lists = lazy(() =>
+    import(/* webpackChunkName: 'Lists'*/ "./pages/Lists")
+);
+const ListDetail = lazy(() =>
+    import(/* webpackChunkName: 'ListDetail'*/ "./pages/ListDetail")
+);
+const ListForm = lazy(() =>
+    import(/* webpackChunkName: 'ListForm'*/ "./pages/ListForm")
+);
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -31,19 +38,21 @@ function App() {
             <AppWrapper>
                 <BrowserRouter>
                     <Header />
-                    <AppContext>
-                        <Routes>
-                            <Route path="/" element={<Lists />} />
-                            <Route
-                                path="/list/:listId/new"
-                                element={<ListForm />}
-                            />
-                            <Route
-                                path="/list/:listId"
-                                element={<ListDetail />}
-                            />
-                        </Routes>
-                    </AppContext>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <AppContext>
+                            <Routes>
+                                <Route path="/" element={<Lists />} />
+                                <Route
+                                    path="/list/:listId/new"
+                                    element={<ListForm />}
+                                />
+                                <Route
+                                    path="/list/:listId"
+                                    element={<ListDetail />}
+                                />
+                            </Routes>
+                        </AppContext>
+                    </Suspense>
                 </BrowserRouter>
             </AppWrapper>
         </>
